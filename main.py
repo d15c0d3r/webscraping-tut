93a45ccbf7eb4f52a310aed34f20a0aa89957f0d
thatviper <challanaganarasimhareddy@gmail.com>
2021-12-01 21:32:40 +0530

    names & prices.scraping

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ with open('home.html','r') as html_file:
     soup = BeautifulSoup(content,'lxml')
     courses_html_tags = soup.find_all('h5')
     #print(courses_html_tags)
-    for course in courses_html_tags:
-        print(course.text)
+    #for course in courses_html_tags:
+        #print(course.text)
+    course_cards = soup.find_all('div',class_ = 'card')
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        print(course_name,'costs',course_price)
