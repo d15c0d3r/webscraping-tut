6256dab522226205b9f5dc10d0aa7c706513ea62
thatviper <challanaganarasimhareddy@gmail.com>
2021-12-03 23:10:14 +0530

    scrape : using condition

--- scrape_1.py
+++ scrape_1.py
@@ -5,20 +5,27 @@ html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?se
 #print(html_text)
 
 soup = BeautifulSoup(html_text,'lxml')
-job = soup.find('li',class_ = 'clearfix job-bx wht-shd-bx')
-company_name = job.find('h3',class_ = 'joblist-comp-name').text
-lst = list(map(str,company_name.split()))
-if(lst[-2]+lst[-1] == '(MoreJobs)'):
-    company_name = ' '.join(lst[:len(lst)-2])
-else:
-    company_name = ' '.join(lst)
-#print(company_name)
+jobs = soup.find_all('li',class_ = 'clearfix job-bx wht-shd-bx')
+for job in jobs:
+    #print(job.text,end = '\n')
+    publish_date = job.find('span',class_ = 'sim-posted').text.strip()
+    if(publish_date in ['Posted today','Posted few days ago']):
+        company_name = job.find('h3',class_ = 'joblist-comp-name').text
+        lst = list(map(str,company_name.split()))
+        if(lst[-2]+lst[-1] == '(MoreJobs)'):
+            company_name = ' '.join(lst[:len(lst)-2])
+        else:
+            company_name = ' '.join(lst)
+    #print(company_name)
 
-skills_str = job.find('span',class_ = 'srp-skills').text.replace(' ','').replace('\r\n','')
-skills = list(map(str,skills_str.split(',')))
-skills_str = ', '.join(skills).strip()
-print(f'''
-    Company Name : {company_name}
-    Skills       : {skills_str}
-''')
+        skills_str = job.find('span',class_ = 'srp-skills').text.replace(' ','').replace('\r\n','')
+        skills = list(map(str,skills_str.split(',')))
+        skills_str = ', '.join(skills).strip()
+        #print(skills)
+
+        print(f'''
+            Company Name     : {company_name}
+            Skills Required  : {skills_str}
+        ''')
+
 
