c6a6f7671d84ef235fec457741c120733163da14
thatviper <challanaganarasimhareddy@gmail.com>
2021-12-03 22:56:45 +0530

    scrape : skills

--- scrape_1.py
+++ scrape_1.py
@@ -12,4 +12,13 @@ if(lst[-2]+lst[-1] == '(MoreJobs)'):
     company_name = ' '.join(lst[:len(lst)-2])
 else:
     company_name = ' '.join(lst)
-print(company_name)
+#print(company_name)
+
+skills_str = job.find('span',class_ = 'srp-skills').text.replace(' ','').replace('\r\n','')
+skills = list(map(str,skills_str.split(',')))
+skills_str = ', '.join(skills).strip()
+print(f'''
+    Company Name : {company_name}
+    Skills       : {skills_str}
+''')
+
